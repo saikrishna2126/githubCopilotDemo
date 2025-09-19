b9b40e3c071298d1222f6bef24b10ac8b93c13b0
saikrishna2126 <manchikatlasai21@gmail.com>
2025-09-19 11:38:42 +0530

    reformated

--- css/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Amazon Clone Loaded');
-
-    const button = document.querySelector('.hero button');
-    button.addEventListener('click', () => {
-        alert('Shop Now button clicked!');
-    });
-});
